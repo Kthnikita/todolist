3eaeb9f0fa25759e4e309fe6ec7442d551cca986
Nikita_pal <161755788+Kthnikita@users.noreply.github.com>
2025-04-13 17:54:07 +0530

    Update Navbar.jsx

--- TODOLIST/src/Navbar.jsx
+++ TODOLIST/src/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 function Navbar() {
   return (
-    <div className='h-16 p-5 gap-3 flex justify-start bg-[#48A6A7] items-center'>
+    <div className='h-16 p-5 gap-3 flex justify-start bg-[#48A6A7] items-center '>
     <div className='h-8 w-8 rounded-full bg-[#9ACBD0] '>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoTp8Gf5mIADYi5HBo_cgBWzXDA5USi_P16Q&s" alt="" className='h-8 w-8 rounded-full '/>
     </div>
-    <h2 className='font-semibold'>TODO APP</h2>
+    <h2 className='font-semibold text-lg italic text-white '>TODO APP</h2>
     </div>
   )
 }
