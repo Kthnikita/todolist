32dc3f0050fdfc3e4bbff31be06449b0e507b419
Nikita_pal <161755788+Kthnikita@users.noreply.github.com>
2025-04-13 17:52:42 +0530

    Add files via upload

---
+++ TODOLIST/src/Modify.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Modify({ todo, settodos, close }) {
+  const [title, settitle] = useState(todo.title);
+  const [description, setdes] = useState(todo.description);
+  function edit() {
+    const updatedTime = new Date().toLocaleString();
+    settodos((prev) => {
+      const uptodo = prev.map((i) => {
+        if (i.id === todo.id) {
+          return { ...i, title, description ,time:updatedTime};
+        }
+        return i;
+      });
+      localStorage.setItem("todosList", JSON.stringify(uptodo));
+      return uptodo;
+    });
+    close();
+  }
+
+  return (
+    <div className='fixed inset-0 z-50 top-0 left-0 w-full h-full bg-black/ backdrop-blur-sm flex justify-center items-center' onClick={close}>
+      <div className='h-[40vh] relative w-[60vh] bg-[#9ACBD0] rounded-md p-5 flex flex-col gap-5 items-center' onClick={(e) => e.stopPropagation()}>
+        <button
+          className='absolute top-0 right-0 text-white bg-blue-500 rounded-full w-4 h-4 flex items-center justify-center text-sm font-bold shadow-md transition'
+          onClick={close}
+        >x</button>
+        <input value={title} onChange={(e) => settitle(e.target.value)} type="text" className='h-8 w-full p-3' placeholder='Enter todo title..' />
+        <textarea value={description} onChange={(e) => setdes(e.target.value)} className='h-40 p-3 w-full' placeholder='Add Description...'></textarea>
+        <button className='h-8 w-20 border border-black bg-[#48A6A7] rounded' onClick={edit}>Save Changes</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modify;
